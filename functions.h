37aef8adf7b23b572421667405a1d8a5765f1ca4
MonteClements97 <33125712+MonteClements97@users.noreply.github.com>
2017-10-29 12:04:50 +0000

    Added search by title
    Search by title has been implemented. Additional comments have been scattered.

--- functions.h
+++ functions.h
@@ -9,4 +9,5 @@ typedef struct s_book {
 book * create_node(char title[], char author[], long isbn);
 book *  insert_node(book * head, book * current_node, book * node_to_insert);
 void print_nodes(book * current_node);
+void print_selected_node(book * head, char title[]);
 #endif //SECONDPRACPROJECTDEBUG_TEST_H
--- main.c
+++ main.c
@@ -10,13 +10,23 @@ int main() {
     head = insert_node(head, head, create_node("Tom", "Tommy", 753216));
     head = insert_node(head, head, create_node("Steve", "Stevey", 983418));
     head = insert_node(head, head, create_node("Ton", "Tony", 835636));
-    head = insert_node(head, head, create_node("Mark", "Marky", 176549));
+    head = insert_node(head, head, create_node("Mont", "Monty", 176549));
+    head = insert_node(head, head, create_node("Jam", "Jammy", 456345));
+    head = insert_node(head, head, create_node("Con", "Conny", 178432));
     print_nodes(head);
+    print_selected_node(head, "Chris");
+    print_selected_node(head, "Mont");
+    print_selected_node(head, "Jammy");
     return 0;
 }
 
 book *create_node(char title[], char author[], long isbn) {
+    //creates a new node with the title, author and isbn passed to it
     book *new_node = malloc(sizeof(book));
+    if (!new_node){      //very unlikely, but just in case, you never know
+        printf("ASSIGNING MEMORY FAILED");
+        exit(2);         //kill the program, not able to deal with this
+    }
     strcpy(new_node->book_title, title);
     strcpy(new_node->book_author, author);
     new_node->book_isbn = isbn;
@@ -24,21 +34,24 @@ book *create_node(char title[], char author[], long isbn) {
 }
 
 book *insert_node(book *head, book *current_node, book *node_to_insert) {
+    //sorts nodes in ascending order
     if (!head) {
         return node_to_insert; //if head is NULL nothing is in the list
     }
 
-    if ((current_node == head) && (current_node->book_isbn < node_to_insert->book_isbn)) {
+    if ((current_node == head) && (current_node->book_isbn > node_to_insert->book_isbn)) {
+        //node that is to be inserted needs to be placed at head
         head = node_to_insert;
         node_to_insert->pointer = current_node;
         return head;
     }
 
     while (current_node) {
+        //while we have something to look at. We should always be looking at something, so essentially a while true
         if (current_node->pointer) {
-            if (current_node->pointer->book_isbn > node_to_insert->book_isbn) {
+            if (current_node->pointer->book_isbn < node_to_insert->book_isbn) {
                 current_node = current_node->pointer;
-                continue;
+                continue; //do not want to hit line 57 if this is the case
             } else {
                 node_to_insert->pointer = current_node->pointer;
                 current_node->pointer = node_to_insert;
@@ -50,7 +63,22 @@ book *insert_node(book *head, book *current_node, book *node_to_insert) {
 
     }
 
-    return head;
+    return head; //just in case something breaks
+}
+
+
+void print_selected_node(book * head, char title[]){
+    //finds a node based on it's title
+    if (!(strcmp(head -> book_title, title))){
+        printf("Title of the book: %s\nName of the author: %s\nISBN: %li\n",
+               head->book_title, head->book_author, head->book_isbn);
+        return;
+    } else if (head->pointer){
+        print_selected_node(head->pointer, title);
+    } else {
+        printf("COULD NOT FIND: %s\n", title);
+    }
+    return;
 }
 
 
@@ -58,9 +86,10 @@ void print_nodes(book *current_node) {
     if (current_node->pointer) {
         print_nodes(current_node->pointer);
     }
-    if (current_node->book_isbn) {
+    if (current_node){
         printf("Title of the book: %s\nName of the author: %s\nISBN: %li\n", current_node->book_title,
                current_node->book_author, current_node->book_isbn);
     }
+
     return;
 }
