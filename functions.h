2b0ddf8a7b62fb53110d906c13a7155cc0d33911
MonteClements97 <33125712+MonteClements97@users.noreply.github.com>
2017-10-29 01:47:46 +0100

    Add files via upload

--- functions.h
+++ functions.h
@@ -7,6 +7,6 @@ typedef struct s_book {
     struct s_book * pointer;
 } book;
 book * create_node(char title[], char author[], long isbn);
-void insert_node(book * current_node, book * node_to_insert);
+book *  insert_node(book * head, book * current_node, book * node_to_insert);
 void print_nodes(book * current_node);
 #endif //SECONDPRACPROJECTDEBUG_TEST_H
